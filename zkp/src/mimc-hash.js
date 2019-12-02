ba5024bec9f223debfdd561dde77c1938c42a8bf
westlad <duncan.westland@uk.ey.com>
2019-12-02 09:21:42 +0000

    feat(zkp): add mimc hash functions

--- zkp/src/mimc-hash.js
+++ zkp/src/mimc-hash.js
@@ -3,6 +3,7 @@ Implements a mimcHash function, mirroring that written by HarryR in Solidity.
 */
 
 // TODO - move this into zkpUtils?
+<<<<<<< HEAD
 import config from 'config';
 import utils from './zkpUtils';
 
@@ -18,6 +19,24 @@ function powerMod(base, exponent, m) {
   while (e > BigInt(0)) {
     if (e % BigInt(2) === BigInt(1)) result = (result * b) % m;
     e >>= BigInt(1);
+=======
+import config from './config';
+import utils from './zkpUtils';
+
+function addMod(addMe, m = config.ZOKRATES_PRIME) {
+  const sum = addMe.reduce((e, acc) => e + acc);
+  return ((sum % m) + m) % m;
+}
+
+function powerMod(base, exponent, m = config.ZOKRATES_PRIME) {
+  if (m === 1) return 0;
+  let result = 1;
+  let b = base % m;
+  let e = exponent;
+  while (e > 0) {
+    if (e % 2 === 1) result = (result * b) % m;
+    e >>= 1;
+>>>>>>> feat(zkp): add mimc hash functions
     b = (b * b) % m;
   }
   return result;
@@ -35,6 +54,7 @@ function mimcpe7(x, k, seed, roundCount, m) {
   let t;
   let c = seed;
   for (let i = 0; i < roundCount; i++) {
+<<<<<<< HEAD
     c = utils.keccak256Hash(c);
     t = addMod([xx, BigInt(c), k], m); // t = x + c_i + k
     xx = powerMod(t, BigInt(7), m); // t^7
@@ -48,10 +68,25 @@ function mimcpe7mp(x, k, seed, roundCount, m = BigInt(config.ZOKRATES_PRIME)) {
   let i;
   for (i = 0; i < x.length; i++) {
     r = (r + (x[i] % m) + mimcpe7(x[i], r, seed, roundCount, m)) % m;
+=======
+    c = utils.hash(c); // TODO
+    t = addMod([xx, c, k], m); // t = x + c_i + k
+    xx = powerMod(t, 7, m); // t^7
+  }
+  // Result adds key again as blinding factor
+  return addMod(xx, k);
+}
+
+function mimcpe7mp(x, k, seed, roundCount, m = config.ZOKRATES_PRIME) {
+  let r = k;
+  for (let i = 0; i < x.length; i++) {
+    r = (r + x[i] + mimcpe7(x[i], r, seed, roundCount)) % m;
+>>>>>>> feat(zkp): add mimc hash functions
   }
   return r;
 }
 
+<<<<<<< HEAD
 function mimcHash(...msgs) {
   // elipses means input stored in array called msgs
   const mimc = '0x6d696d63'; // this is 'mimc' in hex as a nothing-up-my-sleeve seed
@@ -68,6 +103,10 @@ function mimcHash(...msgs) {
   )
     .toString(16) // hex string - can remove 0s
     .padStart(64, '0')}`; // so pad
+=======
+function mimcHash(msgs) {
+  return mimcpe7mp(msgs, 0, utils.sha256Hash('mimc'), 91);
+>>>>>>> feat(zkp): add mimc hash functions
 }
 
 export default { mimcHash };
