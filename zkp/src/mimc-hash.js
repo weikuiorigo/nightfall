a7efa561b23b697d212d5826f36e7642431c0c36
westlad <duncan.westland@uk.ey.com>
2019-12-08 16:42:18 +0000

    feat(zkp): intermediate save

--- zkp/src/mimc-hash.js
+++ zkp/src/mimc-hash.js
@@ -26,13 +26,7 @@ function mod(a, m) {
 }
 
 function addMod(addMe, m) {
-  const sum = addMe.reduce((e, acc) => mod(e + acc, m), BigInt(0));
-  return mod(sum, m);
-}
-
-function mulMod(timesMe, m) {
-  const product = timesMe.reduce((e, acc) => mod(e * acc, m), BigInt(1));
-  return mod(product, m);
+  return addMe.reduce((e, acc) => mod(e + acc, m), BigInt(0));
 }
 
 function powerMod(base, exponent, m) {
@@ -42,7 +36,7 @@ function powerMod(base, exponent, m) {
   let e = exponent;
   while (e > BigInt(0)) {
     if (e % BigInt(2) === BigInt(1)) result = mod(result * b, m);
-    e /= BigInt(2);
+    e >>= BigInt(1);
     b = mod(b * b, m);
 >>>>>>> feat(zkp): debugging mimc hash
   }
@@ -60,10 +54,9 @@ function mimcpe7(x, k, seed, roundCount, m) {
   let xx = x;
   let t;
   let c = seed;
-  let a;
-  console.log('roundCount', roundCount);
   for (let i = 0; i < roundCount; i++) {
     c = utils.keccak256Hash(c);
+<<<<<<< HEAD
 <<<<<<< HEAD
     t = addMod([xx, BigInt(c), k], m); // t = x + c_i + k
     xx = powerMod(t, BigInt(7), m); // t^7
@@ -72,35 +65,42 @@ function mimcpe7(x, k, seed, roundCount, m) {
   return addMod([xx, k], m);
 =======
     // console.log(xx,BigInt(`0x${c}`),k);
+=======
+>>>>>>> feat(zkp): intermediate save
     t = addMod([xx, BigInt(`0x${c}`), k], m); // t = x + c_i + k
-    // console.log('sum', (xx + BigInt(`0x${c}`) + k) % m);
     xx = powerMod(t, BigInt(7), m); // t^7
-    a = mulMod([t, t], m);
-    const yy = mulMod([mulMod([mulMod([a, a], m), a], m), t], m);
-    // console.log('different', xx, yy);
   }
   // Result adds key again as blinding factor
+<<<<<<< HEAD
   // return addMod([xx, k], m);
   // console.log('c', c)
   console.log('t', t);
   return a;
   //return BigInt(`0x${c}`);
 >>>>>>> feat(zkp): debugging mimc hash
+=======
+  return addMod([xx, k], m);
+>>>>>>> feat(zkp): intermediate save
 }
 
 function mimcpe7mp(x, k, seed, roundCount, m = BigInt(config.ZOKRATES_PRIME)) {
   let r = k;
+<<<<<<< HEAD
 <<<<<<< HEAD
   let i;
   for (i = 0; i < x.length; i++) {
     r = (r + (x[i] % m) + mimcpe7(x[i], r, seed, roundCount, m)) % m;
 =======
   for (let i = 0; i < x.length; i++) {
+=======
+  let i;
+  for (i = 0; i < x.length; i++) {
+>>>>>>> feat(zkp): intermediate save
     r = (r + x[i] + mimcpe7(x[i], r, seed, roundCount, m)) % m;
 >>>>>>> feat(zkp): debugging mimc hash
   }
-  return mimcpe7(x[0], k, seed, roundCount, m);
-  // !return r;
+  return k + x[0] + mimcpe7(x[0], k, seed, roundCount, m);
+  // return r;
 }
 
 <<<<<<< HEAD
@@ -121,8 +121,12 @@ function mimcHash(...msgs) { //elipses means input stored in array called msgs
 =======
 function mimcHash(msgs) {
   const mimc = '0x6d696d63'; // this is 'mimc' in hex as a nothing-up-my-sleeve seed
+<<<<<<< HEAD
   return mimcpe7mp(msgs.map(e => BigInt(e)), BigInt(0), utils.keccak256Hash(mimc), 12); // 91
 >>>>>>> feat(zkp): debugging mimc hash
+=======
+  return mimcpe7mp(msgs.map(e => BigInt(e)), BigInt(0), utils.keccak256Hash(mimc), 91); // 91
+>>>>>>> feat(zkp): intermediate save
 }
 
 export default { mimcHash };
